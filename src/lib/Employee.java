790e05b836e14a3fcd2b0f0a1e03d9950c9f6a08
jahfal21 <jahfalramadhan4@gmail.com>
2023-04-14 20:34:14 +0700

    menghapus comments yang tidak diperlukan pada class Employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -18,13 +18,6 @@ public class Employee {
 		this.isForeigner = isForeigner;
 	}
 
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya
-	 * (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3:
-	 * 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-
 	// Long Method
 	public void setMonthlySalary(int grade) {
 		switch (grade) {
