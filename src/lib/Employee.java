d12101bcb2934e73f1af8f3e6a30bc0a69d0af9b
jahfal21 <jahfalramadhan4@gmail.com>
2023-04-14 19:35:17 +0700

    merefactor method setMonthlySalary menggunakan statement switch

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -25,22 +25,25 @@ public class Employee {
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 
+	// Long Method
 	public void setMonthlySalary(int grade) {
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		} else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		} else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		switch (grade) {
+			case 1:
+				monthlySalary = 3000000;
+				break;
+			case 2:
+				monthlySalary = 5000000;
+				break;
+			case 3:
+				monthlySalary = 7000000;
+				break;
+			default:
+				monthlySalary = 0;
+				break;
+		}
+
+		if (isForeigner) {
+			monthlySalary *= 1.5;
 		}
 	}
 
